3e3451433c1525a4772defb3cfe90ad7ad6b2c89
xzhang682 <xzhang682@wisc.edu>
2016-04-16 18:32:16 -0500

    Update Comment.java

--- src/entity/Comment.java
+++ src/entity/Comment.java
@@ -1,5 +1,47 @@
 package entity;
 
 public class Comment {
-
+    private int commentID;
+    private String mytime;
+    private String content;
+    private int score;
+    
+    public Comment (int commentID, String mytime, String content, int score){
+      this.commentID = commentID;
+      this.mytime = mytime;
+      this.content = content;
+      this.score = score;
+    }
+    
+    public int getCommentID(){
+      return commentID;
+    }
+    
+    public void setCommentID(commentID){
+      this.commentID = commentID;
+    }
+    
+    public String getMytime(){
+      return mytime;
+    }
+    
+    public void setMytime(mytime){
+      this.mytime = mytime;
+    }
+    
+        public String getContent(){
+      return content;
+    }
+    
+    public void setContent(content){
+      this.content = content;
+    }
+    
+    public int getScore(){
+      return score;
+    }
+    
+    public void setScore(score){
+      this.score = score;
+    }
 }
