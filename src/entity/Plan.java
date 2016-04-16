81fcb214b961e4004ee1e0c6669c343821fef274
xzhang682 <xzhang682@wisc.edu>
2016-04-16 17:13:36 -0500

    Update Plan.java

--- src/entity/Plan.java
+++ src/entity/Plan.java
@@ -12,6 +12,103 @@ public class Plan {
 	private String destination;
     	private String title;
     	private int maxPeople;
-    	private int having People;
+    	private int havingPeople;
 	private int mylike;
+	
+	public Plan(int planID, Date mytime_start, Date mytime_end, int cost, String content, String coverPath,
+			String destination, String title, int maxPeople, int havingPeople, int mylike){
+		super();
+		this.planID = planID;
+		this.mytime_start = mytime_start;
+		this.mytime_end = mytime_end;
+		this.cost = cost;
+		this.content = content;
+		this.coverPath = coverPath;
+		this.destination = destination;
+		this.title = title;
+		this.maxPeople = maxPeople;
+		this.havingPeople = havingPeople;
+		this.mylike = mylike;
+	}
+	
+	public int getPlanID() {
+		return planID;
+	}	
+
+	public void setPlanID(String planID) {
+		this.planID = planID;
+	}
+
+	public Date getMytime_start() {
+		return mytime_start;
+	}
+
+	public void setMytime_start(Date mytime_start) {
+		this.mytime_start = mytime_start;
+	}
+
+	public Date getMytime_end() {
+		return mytime_end;
+	}
+
+	public void setMytime_end(Date mytime_end) {
+		this.mytime_end = mytime_end;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+	public String getContent() {
+		return birthday;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getCoverPath() {
+		return coverPath;
+	}
+
+	public void setCoverPath(String coverPath) {
+		this.coverPath= coverPath;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getMaxPeople() {
+		return maxPeople;
+	}
+
+	public void setMaxPeople(int maxPeople) {
+		this.maxPeople = maxPeople;
+	}
+
+	public int getHavingPeople() {
+		return havingPeople;
+	}
+
+	public void setMylike(int mylike) {
+		this.mylike = mylike;
+	}
+	
 }
