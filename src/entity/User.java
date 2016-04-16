6b68d4d04d872cf78dda6e8f4497e4f21f4c9b92
liliumao <371812917@qq.com>
2016-04-16 15:15:41 -0500

    Backend  set up

---
+++ src/entity/User.java
@@ -0,0 +1,107 @@
+package entity;
+
+import java.sql.Date;
+import java.util.List;
+
+/**
+ * Created by m on 2015/6/6.
+ */
+public class User {
+	private int userID;
+	private String email;
+	private String phoneNum;
+	private String username;
+	private String profileURL;
+	private Date birthday;
+	private int followerCount;
+	private int followingCount;
+	private List<User> follower;
+
+	public User(int userID, String email, String phoneNum, String username,
+			String profileURL, Date birthday, int followerCount,
+			int followingCount) {
+		super();
+		this.userID = userID;
+		this.email = email;
+		this.phoneNum = phoneNum;
+		this.username = username;
+		this.profileURL = profileURL;
+		this.birthday = birthday;
+		this.followerCount = followerCount;
+		this.followingCount = followingCount;
+	}
+
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+
+	public String getProfileURL() {
+		return profileURL;
+	}
+
+	public void setProfileURL(String profileURL) {
+		this.profileURL = profileURL;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getFollowerCount() {
+		return followerCount;
+	}
+
+	public void setFollowerCount(int followerCount) {
+		this.followerCount = followerCount;
+	}
+
+	public int getFollowingCount() {
+		return followingCount;
+	}
+
+	public void setFollowingCount(int followingCount) {
+		this.followingCount = followingCount;
+	}
+
+	public List<User> getFollower() {
+		return follower;
+	}
+
+	public void setFollower(List<User> follower) {
+		this.follower = follower;
+	}
+	
+	
+}
