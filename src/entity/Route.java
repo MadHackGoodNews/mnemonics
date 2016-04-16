b8b2015df140140501a085cd6bac099fff257478
xzhang682 <xzhang682@wisc.edu>
2016-04-16 18:07:35 -0500

    Update Route.java

--- src/entity/Route.java
+++ src/entity/Route.java
@@ -1,5 +1,137 @@
 package entity;
 
 public class Route {
+  private int routeID;
+  private int userID;
+  private char
+      routeID integer not null auto_increment,
+    userID integer,
+    mytime time,
+    destination varchar(20) not null,
+    cost integer,
+    coverPath varchar(50),
+    tag varchar(100),
+    title varchar(50),
+    score real,
+    primary key(routeID),
+    foreign key (userId) references users(userId)
+package entity;
+
+import java.sql.Date;
+
+/**
+ * Created by m on 2015/6/6.
+ */
+public class User {
+	private int userID;
+	private String email;
+	private String phoneNum;
+	private String username;
+	private String password;
+	private String profileURL;
+	private String intro;
+	private Date birthday;
+	private int followerCount;
+	private int followingCount;
+
+
+	public User(int userID, String email, String phoneNum, String username,
+			String password, String profileURL, String intro, Date birthday,
+			int followerCount, int followingCount) {
+		super();
+		this.userID = userID;
+		this.email = email;
+		this.phoneNum = phoneNum;
+		this.username = username;
+		this.password = password;
+		this.profileURL = profileURL;
+		this.intro = intro;
+		this.birthday = birthday;
+		this.followerCount = followerCount;
+		this.followingCount = followingCount;
+	}
+
+	public String getIntro() {
+		return intro;
+	}
+
+	public void setIntro(String intro) {
+		this.intro = intro;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+
+	public String getProfileURL() {
+		return profileURL;
+	}
+
+	public void setProfileURL(String profileURL) {
+		this.profileURL = profileURL;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getFollowerCount() {
+		return followerCount;
+	}
+
+	public void setFollowerCount(int followerCount) {
+		this.followerCount = followerCount;
+	}
+
+	public int getFollowingCount() {
+		return followingCount;
+	}
+
+	public void setFollowingCount(int followingCount) {
+		this.followingCount = followingCount;
+	}
+
+	
+}
 
 }
