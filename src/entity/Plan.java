d5d7120df02a08c9b4873192a4895ca84cd91caf
xzhang682 <xzhang682@wisc.edu>
2016-04-16 16:19:45 -0500

    Update Plan.java

--- src/entity/Plan.java
+++ src/entity/Plan.java
@@ -4,7 +4,14 @@ import java.sql.Date;
 
 public class Plan {
 	private int planID;
-	private Date depTime;
-	private Date arvTime;
-	
+	private Date mytime_start;
+	private Date mytime_end;
+	private int cost;
+	private String content;
+	private String coverPath;
+	private String destination;
+    	private String title;
+    	private int maxPeople;
+    	private int having People;
+	private int mylike;
 }
